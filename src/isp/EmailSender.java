34c2a1a617757c0beb8234a093ed68df07896f90
Urtzi Espinosa <157757457+Urtzi78@users.noreply.github.com>
2024-10-20 13:40:07 +0200

    Add files via upload

--- src/isp/EmailSender.java
+++ src/isp/EmailSender.java
@@ -3,6 +3,6 @@ package isp;
 public class EmailSender {
 	public static void sendEmail(EmailReceiver er, String message) {
 		String email = er.getEmail();
-        System.out.println("Emailaren igorlea: " + email + "\nMezua: " + message);
+        	System.out.println("Emailaren igorlea: " + email + "\nMezua: " + message);
 	}
 }
